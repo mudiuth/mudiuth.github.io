220371695ac3cce039b2626d1a4654441f5429f5
pages uploaded <mudiuth@gmail.com>
2026-04-25 16:14:47 +0300

    cleaning the repo

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9e2d7a84.js',
+                path_to_root + 'searchindex-03067677.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
