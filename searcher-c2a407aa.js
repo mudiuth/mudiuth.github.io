e57f57dc98b54c3ee4e90950a083742a774331be
pages uploaded <mudiuth@gmail.com>
2026-04-28 21:02:53 +0300

    adding the CAS walkthrough

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-03067677.js',
+                path_to_root + 'searchindex-4e2f1789.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
