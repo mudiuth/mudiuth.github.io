0931bc40c7a6f8debd98c22c89e9a09d6574626f
r00t3r <r00t3r@S3mu.semu.local>
2026-05-05 08:57:29 +0300

    changed style

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4e2f1789.js',
+                path_to_root + 'searchindex-6384d84c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
